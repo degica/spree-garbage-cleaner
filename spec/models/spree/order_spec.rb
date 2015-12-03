8a3dc46707b4b3be60b09a7ca9853493c3bbf58e
Chris Salzberg <csalzberg@degica.com>
2015-12-03 15:56:23 +0900

    Use ruby 2.1.7

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -29,22 +29,22 @@ describe Spree::Order do
     end
 
     it "is garbage if not completed and past cleanup_days_interval" do
-      order = Factory.build(:order, :updated_at => ordered_on.days.ago, :completed_at => nil)
+      order = Factory.build(:order, updated_at: ordered_on.days.ago, completed_at: nil)
       order.garbage?.should be_true
     end
 
     it "is not garbage if not completed and not past cleanup_days_interval" do
-      order = Factory.build(:order, :updated_at => (ordered_on-1).days.ago, :completed_at => nil)
+      order = Factory.build(:order, updated_at: (ordered_on-1).days.ago, completed_at: nil)
       order.garbage?.should be_false
     end
 
     it "is not garbage if completed and past cleanup_days_interval" do
-      order = Factory.build(:order, :updated_at => ordered_on.days.ago, :completed_at => Time.now)
+      order = Factory.build(:order, updated_at: ordered_on.days.ago, completed_at: Time.now)
       order.garbage?.should be_false
     end
 
     it "is not garbage if completed and not past cleanup_days_interval" do
-      order = Factory.build(:order, :completed_at => Time.now)
+      order = Factory.build(:order, completed_at: Time.now)
       order.garbage?.should be_false
     end
   end
